c9fc05acf711d445e113f4aba824f56e20a24a84
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-18 13:17:21 -0300

    adicionando projeto todo react native, atualizando imagens

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -4,6 +4,8 @@ import trybetunesImg from '../imgs/trybetunes.png'
 import todolistImg from '../imgs/todolist.png'
 import triviaImg from '../imgs/trivia.png'
 import recipesImg from '../imgs/recipes.png'
+import tfcImg from '../imgs/tfcImg.png'
+import todoNativeImg from '../imgs/todoNative.png'
 
 
 class Projects extends Component {
@@ -25,10 +27,13 @@ class Projects extends Component {
           <img src={trybetunesImg} alt="imagem trybetunes" />
           </a>
           <a href="https://frontend-production-5124.up.railway.app/leaderboard" target="_blank" className="ext" rel="noreferrer" >
-          <img src="https://raw.githubusercontent.com/lcsrbr/TrybeFutebolClube/main/imgs/login.png" alt="imagem TFC" />
+          <img src={tfcImg} alt="imagem TFC" />
           </a>
           <a href="https://recipes-app-red.vercel.app/" target="_blank" className="ext" rel="noreferrer" >
           <img src={recipesImg} alt="imagem recipesApp" />
+          </a>
+          <a href="https://codesandbox.io/s/misty-river-if9ms1?file=/src/App.js&resolutionWidth=320&resolutionHeight=675" >
+          <img src={todoNativeImg} alt="imagem todolistNative" />
           </a>
         </div>
       </section>
