5123980f2c54d10f5b6f956d8d55482af8783418
Lucas Moura <93lucasribeiro@gmail.com>
2023-02-07 17:57:00 -0300

    estilização do header, responsividade e menu sanduiche TOP DO TOP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 import * as S from './styles';
 
 function Header() {
-  const [button, setButton] = useState(true);
+  const [toggle, setToggle] = useState(false);
+  const [animated, setAnimated] = useState('container');
+
+  const handleButton = () => {
+    setToggle(!toggle);
+    setAnimated(animated === 'container' ? 'change container' : 'container');
+  };
 
   return (
     <S.Header>
@@ -11,14 +17,20 @@ function Header() {
         <h1>{'<LucasMoura/>'}</h1>
       </Link>
 
-      <S.Button open={button} onClick={() => setButton(!button)}>button</S.Button>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/work">Work</Link>
-        <Link to="/contact">Contact</Link>
-        <p>☀️</p>
-      </nav>
+      <S.Nav open={toggle}>
+        <button onClick={handleButton} className={animated}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </button>
+        <nav>
+          <Link to="/">Início</Link>
+          <Link to="/sobre">Sobre</Link>
+          <Link to="/projetos">Projetos</Link>
+          <Link to="/contato">Contato</Link>
+          <p>☀️</p>
+        </nav>
+      </S.Nav>
     </S.Header>
   );
 }
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,13 +1,22 @@
 import styled from 'styled-components';
 import colors from '../styles/colors';
 export const Header = styled.div`
+@media only screen and (max-width: 800px) {
+  h1 {
+    font-size: 25px;
+    z-index: -3;
+    }
+  }
+
   display: flex;
   width: 100%;
   position: fixed;
+  z-index: 3;
   align-items: center;
-  gap: 40rem;
-  justify-content: center;
-  background-color: ${colors.background2};
+  // gap: 40rem;
+  justify-content: space-between;
+  padding:  0px 1vh 0px 1vh;
+  background-color: ${colors.background1};
   height: 70px;
   h1 {
     color: ${colors.font1};
@@ -15,16 +24,74 @@ export const Header = styled.div`
   h1: hover {
     color: ${colors.font2}};
   }
-  nav {
-    display: flex;
-    gap: 1rem;;
-    align-items: center;
-  }
-  nav a: hover {
-    color: ${colors.font1};
-}
 `;
 
-export const Button = styled.button<{ open: boolean }>`
-  display: ${({ open }) => (open ? `none` : `fixed`)};
+export const Nav = styled.div<{ open: boolean }>`
+  button {
+    border: none;
+    background-color: transparent;
+  }
+  justify-content: center;
+  nav {
+    display: flex;
+    gap: 1rem;
+  }
+  a: hover {
+    color: ${colors.font1};
+  }
+
+  @media only screen and (min-width: 800px) {
+    button {
+      display: none;
+    }
+  }
+
+  .bar1,
+  .bar2,
+  .bar3 {
+    width: 25px;
+    height: 2px;
+    background-color: ${colors.font1};
+    margin: 2px 0;
+    transition: 0.4s;
+  }
+
+  .change .bar1 {
+    transform: translate(0, 5px) rotate(-45deg);
+  }
+
+  .change .bar2 {
+    opacity: 0;
+  }
+
+  .change .bar3 {
+    transform: translate(0, -3px) rotate(45deg);
+  }
+  @media only screen and (max-width: 800px) {
+    display: flex;
+    align-items: center;
+
+    button: {
+      position: absolute;
+      display: inline-block;
+      cursor: pointer;
+      z-index: 2;
+    }
+
+    nav {
+      align-items: center;
+      transform: ${({ open }) => (open ? `translateX(0)` : `translateX(100%)`)};
+      background-color: ${({ open }) => open && colors.background1};
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      width: 30%;
+      height: 100vh;
+      position: fixed;
+      z-index: -1;
+      top: 0;
+      right: 0;
+    }
+  }
 `;
